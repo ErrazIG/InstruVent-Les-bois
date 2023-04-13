8fa8ba5780724346325b1e0755889c7c7853617a
Maksym Sokol <112185430+Maksym995@users.noreply.github.com>
2023-04-13 15:41:25 +0200

    Contact_v2

---
+++ model/MailModel.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * fonction pour envoyer des mails
+ * @param string $mailfrom
+ * @param string $mailto
+ * @param string $subject
+ * @param string $message
+ * @return bool
+ */
+function sendMail(string $mailfrom, string $mailto, string $subject, string $message): bool {
+
+
+    // création des entêtes
+    $headers = array(
+        'From' => $mailfrom,
+        'Reply-To' => $mailfrom,
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+
+    // envoi réel du mail
+    $envoi = @mail($mailto, $subject, $message, $headers);
+    // mail envoyé
+    if($envoi){
+        // message de réussite de l'envoi
+        $sended = "Votre message a bien été envoyé";
+        return $sended;
+    }else {
+        // problème de la fonction mail, erreur personnalisée
+        $sended = "Il y a eu un problème, veuillez réessayer.";
+        return $sended;
+    }
+}
