b58d807306fc7c39a7068091c7745c13b824d4a5
ErrazIG <frederic-vml@hotmail.com>
2023-04-14 13:00:23 +0200

    IncludeMenuFooterViewAndCss

---
+++ includes/publicMenu.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg nav-zindex" style="width: 100%;position: fixed; background: var(--brown-background);">
+            <div class="container-fluid">
+              <a class="navbar-brand text-warning btn-home fs-4 logo-site" href="?p=Accueil">Instruvent | Bois</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon icon-menu"></span>
+              </button>
+              <div class="collapse navbar-collapse menu" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link mt-2" id="link" href="?p=Accueil">Accueil</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link mt-2" id="link" href="?p=Propos">A propos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link mt-2" id="link" href="?p=Instruments">Instruments</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link mt-2" id="link" href="?p=Contact">Contact</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" id="link" href="">
+                      <button type="button" class="btn btn-warning btn-admin">Admin</button></a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </nav>
