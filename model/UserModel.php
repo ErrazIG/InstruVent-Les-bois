cb259c39bebe4665e1ef65a37fac0e14fbf9d841
Maksym995 <maksymsokoll@gmail.com>
2023-04-17 16:28:09 +0200

    UserModel_V1 _ privateController_V1

--- model/UserModel.php
+++ model/UserModel.php
@@ -8,3 +8,67 @@
  */
 
  
+function getOneUseById(PDO $db, int $uid): array|bool {
+
+    $sql="SELECT userID, username FROM user WHERE userID=?";
+    $prepare=$db->prepare($sql);
+    try{
+        $prepare->execute([$uid]);
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
+    $return = $prepare->fetch(PDO::FETCH_ASSOC);
+    $prepare->closeCursor();
+    return $return;
+    }
+
+    function connectUserByUsername(PDO $db, string $uname, string $pass) :bool|string {
+        // select user, même si le mot de passe n'est pas bon
+    $sql="SELECT * FROM user WHERE username=?";
+    $prepare=$db->prepare($sql);
+    try{
+        $prepare->execute([$uname]);
+    }catch(Exception $e){
+        die($e->getMessage());
+    }
+    if($prepare->rowCount()==1){
+        $response=$prepare->fetch(PDO::FETCH_ASSOC);
+        // est-ce que le mot de passe est dans la DB, crypé et correspond à l'entrée utilisateur
+        if(password_verify($pass,$response['user_pdw'])){
+            // création d'une session qui renvera true
+            // injection dans la variable de session du contenu de la requête
+            $_SESSION=$response;
+            // suppression de variables inutiles dans $_SESSION
+            unset($_SESSION['user_pdw'],$_SESSION['uniqID']);
+            // id utilisée pour créé la variable de session
+            $_SESSION['userID']=session_id();
+
+            return true;
+        }else{
+            return "Login et/ou mot de passe incorrecte"; // Password
+        }
+    }else{
+        return "Login et/ou mot de passe incorrecte"; //Login
+    }
+
+}
+
+// déconnexion
+function deconnect(): bool{
+    # réinitialisation du tableau/variable $_SESSION)
+    $_SESSION = [];
+
+    # suppression du cookie
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    # Destruction du fichier lié sur le serveur
+    session_destroy();
+
+    return true;
+}
