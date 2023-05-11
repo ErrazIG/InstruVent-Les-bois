f1a599a03e40335b20d9e1f6ccefbfe036ce07a3
Youssef Rumblue <youssef_1804@hotmail.fr>
2023-05-11 14:02:55 +0200

    modifComOnPeuOui

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,22 @@
+var audio = document.querySelector(".audio");
+var btn = document.querySelector(".audio-btn");
+var img = document.querySelector("#ficheImg");
+
+btn.style.background = "url(assets/play.png) no-repeat";
+
+function play() {
+    if(audio.paused) {
+        audio.play();
+        btn.style.background = "url(assets/pause.png) no-repeat";
+        img.style.animation = "imgTourne 5s normal linear infinite";
+    }
+
+    else {
+        audio.pause();
+        btn.style.background = "url(assets/play.png) no-repeat";
+        img.style.animation = "none";
+    }
+}
 
 
   
