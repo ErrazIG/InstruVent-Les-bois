1f87ce9a75afc43e797661652b3c1dc0ef7294bc
ErrazIG <Frederic-vml@hotmail.com>
2023-05-12 14:44:30 +0200

    devinez quoi les gars j'ai fais le create

--- controller/privateController.php
+++ controller/privateController.php
@@ -3,21 +3,28 @@
 # echo __FILE__;
 # var_dump($_SESSION);
 
+$allCateg = getAllCategoryMenu($connectPDO);
+
+
 if (isset($_GET['disconnect'])) {
     // si déconnexion renvoie true
     if (deconnect()) {
         // redirection
         header("Location: ./");
         exit();
-    }
-
-    
+    }  
 }
+
 if (isset($_GET['p']) && $_GET['p'] === "Update"){
 
     require_once "../view/privateView/privateUpdate.php";
 
 }elseif (isset($_GET['p']) && $_GET['p'] === "Create"){
+    if(isset($_POST['titre'],$_POST['description'],$_POST['category_instrument_categoryID'])) {
+        $newinstrument = createInstrument($connectPDO,$_POST['titre'], $_POST['description'], $_POST['category_instrument_categoryID']);
+        echo 'Instrument ajouté avec succès';
+    }
+
 
     require_once "../view/privateView/privateCreate.php";
     
@@ -27,4 +34,4 @@ if (isset($_GET['p']) && $_GET['p'] === "Update"){
 }
 
 
-var_dump($_SESSION);
+//var_dump($_SESSION);
